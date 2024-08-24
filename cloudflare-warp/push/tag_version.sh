daaa5df1f9f04af649d65514ca6877f60311c347
Grinch27 <Grinch27@outlook.com>
2024-08-24 23:30:20 +0800

    fix shell script

--- cloudflare-warp/push/tag_version.sh
+++ cloudflare-warp/push/tag_version.sh
@@ -4,5 +4,5 @@
 dir_current=$(dirname "$(readlink -f "$0")")
 echo "dir_current=${dir_current}"
 
-tag_version=$(echo "$tag_version" | awk -F'-' '{print $2}')
+tag_version=$(echo "${tag_version}" | awk -F'-' '{print $2}')
 echo "tag_version=${tag_version}" >> $GITHUB_ENV
