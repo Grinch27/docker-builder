63e1c1905a0e544a5dab7029e3b2a797924c6813
Grinch27 <Grinch27@outlook.com>
2024-08-25 21:01:43 +0800

    Update meta-app.sh

--- expressvpn/meta-app/meta-app.sh
+++ expressvpn/meta-app/meta-app.sh
@@ -30,7 +30,7 @@ OS_VER=$(lsb_release -cs)
 export URL_HTML="https://www.expressvpn.works/latest"
 export FILE_HTML="/latest.html"
 curl -fsSL ${URL_HTML} -o ${FILE_HTML}
-cat ${FILE_HTML}
+# cat ${FILE_HTML}
 
 # 提取带有 data-signature-uri 属性的 <option> 标签的 value 属性并存储到文件中
 cat ${FILE_HTML} | hxselect 'option[data-signature-uri]::attr(value)' >> /tmp/download_links.txt
