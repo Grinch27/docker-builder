e95a24326a6b34b8cdc68796b4f6a474d966bbb1
Grinch27 <Grinch27@outlook.com>
2024-08-25 12:57:29 +0800

    Update tag_version.sh

--- vlmcsd/push/tag_version.sh
+++ vlmcsd/push/tag_version.sh
@@ -5,5 +5,5 @@ dir_current=$(dirname "$(readlink -f "$0")")
 echo "dir_current=${dir_current}"
 
 echo "tag_version=${tag_version}"
-tag_version=$(echo "${tag_version}" | awk -F'-' '{print $2}')
+tag_version=$(echo "${tag_version}" | cut -d'-' -f2-)
 echo "tag_version=${tag_version}"
