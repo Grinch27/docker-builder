cfebfd7e0417164900f80c3f83ecd95860017145
Grinch27 <Grinch27@outlook.com>
2024-08-25 10:36:39 +0800

    Update tag_version.sh

--- cloudflare-warp/push/tag_version.sh
+++ cloudflare-warp/push/tag_version.sh
@@ -4,5 +4,6 @@
 dir_current=$(dirname "$(readlink -f "$0")")
 echo "dir_current=${dir_current}"
 
-tag_version=$(echo '${tag_version}' | awk -F'-' '{print $2}')
+echo "tag_version=${tag_version}"
+tag_version=$(echo "${tag_version}" | awk -F'-' '{print $2}')
 echo "tag_version=${tag_version}"
