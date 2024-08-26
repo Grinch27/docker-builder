b96a6f219e314d5b283b4c727a6ad8c312ec4afa
Grinch27 <Grinch27@outlook.com>
2024-08-26 18:31:09 +0800

    Update

--- adguardvpn-cli/meta-app/meta-app.sh
+++ adguardvpn-cli/meta-app/meta-app.sh
@@ -4,22 +4,38 @@
 dir_current=$(dirname "$(readlink -f "$0")")
 echo "dir_current=${dir_current}"
 
-apk update
+apk update -q
 apk add --no-cache -q \
     ca-certificates \
     curl \
-    tar \
-    gpg
+    grep
 # install.sh
 export URL_SH="https://raw.githubusercontent.com/AdguardTeam/AdGuardVPNCLI/master/scripts/${tag_repo}/install.sh"
 export FILE_SH="/install.sh"
 curl -fsSL ${URL_SH} -o ${FILE_SH}
-sed -i 's/read -r response < \/dev\/tty/response="y"/g' ${FILE_SH}
-chmod +x ${FILE_SH}
-sh --verbose ${FILE_SH}
-rm -f ${FILE_SH}
 
-version=$(adguardvpn-cli --version)
-remove_prefix="AdGuard VPN CLI v"
-version=${version##*$remove_prefix}
+version=$(grep -oP "version='.*?'" ${FILE_SH} | head -1)
+version=$(echo ${version} | awk -F"'" '{print $2}')
 echo "version=${version}"
+
+
+
+# apk update
+# apk add --no-cache -q \
+#     ca-certificates \
+#     curl \
+#     tar \
+#     gpg
+# # install.sh
+# export URL_SH="https://raw.githubusercontent.com/AdguardTeam/AdGuardVPNCLI/master/scripts/${tag_repo}/install.sh"
+# export FILE_SH="/install.sh"
+# curl -fsSL ${URL_SH} -o ${FILE_SH}
+# sed -i 's/read -r response < \/dev\/tty/response="y"/g' ${FILE_SH}
+# chmod +x ${FILE_SH}
+# sh --verbose ${FILE_SH}
+# rm -f ${FILE_SH}
+
+# version=$(adguardvpn-cli --version)
+# remove_prefix="AdGuard VPN CLI v"
+# version=${version##*$remove_prefix}
+# echo "version=${version}"
