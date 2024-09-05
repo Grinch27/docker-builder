25cce00eafa551a2d4ee39a431237b458fa4f65b
Grinch27 <Grinch27@outlook.com>
2024-09-05 15:52:35 +0800

    Update meta-app.sh

--- cloudflare-warp/meta-app/meta-app.sh
+++ cloudflare-warp/meta-app/meta-app.sh
@@ -17,28 +17,31 @@ OS_ARCH=$(dpkg --print-architecture)
 OS_VER=$(lsb_release -cs)
 curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
 echo "deb [arch=${OS_ARCH} signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ ${OS_VER} main" > /etc/apt/sources.list.d/cloudflare-client.list
-
-# Install cloudflare-warp
 apt-get update
-apt-get install -y \
-    cloudflare-warp
 
-# Clean pre-install
-apt-get purge --autoremove -y \
-    curl \
-    lsb-release
+# # Install cloudflare-warp
+# apt-get install -y \
+#     cloudflare-warp
+
+# # Clean pre-install
+# apt-get purge --autoremove -y \
+#     curl \
+#     lsb-release
     
-# Clean apt
-apt-get clean
-rm -rf /var/lib/apt/lists/*
-rm -rf /var/log/*.log
-unset DEBIAN_FRONTEND
+# # Clean apt
+# apt-get clean
+# rm -rf /var/lib/apt/lists/*
+# rm -rf /var/log/*.log
+# unset DEBIAN_FRONTEND
 
-# ENTRYPOINT startapp.sh
-echo "#!/bin/sh\nservice dbus start\n/bin/warp-svc\nwarp-cli disconnect --accept-tos" > /startapp.sh
-chmod +x /startapp.sh
+# # ENTRYPOINT startapp.sh
+# echo "#!/bin/sh\nservice dbus start\n/bin/warp-svc\nwarp-cli disconnect --accept-tos" > /startapp.sh
+# chmod +x /startapp.sh
 
-version=$(warp-cli --version)
-remove_prefix="warp-cli "
-version=${version##*$remove_prefix}
+# version=$(warp-cli --version)
+# remove_prefix="warp-cli "
+# version=${version##*$remove_prefix}
+# echo "version=${version}"
+
+version=$(apt-cache madison cloudflare-warp | awk -F '|' '{print $2}' | tr -d ' ' | head -n 1)
 echo "version=${version}"
