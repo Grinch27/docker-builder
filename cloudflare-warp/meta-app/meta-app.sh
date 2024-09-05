d93eb21912a5d60e57c922df7d8ba972b855e5d6
Grinch27 <Grinch27@outlook.com>
2024-09-05 14:21:14 +0800

    Update meta-app.sh

--- cloudflare-warp/meta-app/meta-app.sh
+++ cloudflare-warp/meta-app/meta-app.sh
@@ -10,9 +10,7 @@ apt-get install -y --no-install-recommends -qq \
     ca-certificates \
     gpg \
     curl \
-    lsb-release \
-    systemd \
-    # dbus \
+    lsb-release
 
 # Add cloudflare gpg key
 OS_ARCH=$(dpkg --print-architecture)
@@ -20,16 +18,16 @@ OS_VER=$(lsb_release -cs)
 curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
 echo "deb [arch=${OS_ARCH} signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ ${OS_VER} main" > /etc/apt/sources.list.d/cloudflare-client.list
 
-# Clean pre-install
-apt-get purge --autoremove -y \
-    curl \
-    lsb-release
-
 # Install cloudflare-warp
 apt-get update
 apt-get install -y \
     cloudflare-warp
 
+# Clean pre-install
+apt-get purge --autoremove -y \
+    curl \
+    lsb-release
+    
 # Clean apt
 apt-get clean
 rm -rf /var/lib/apt/lists/*
