86b0c16f695edd3deb78768acf08332bb5f6bb09
Grinch27 <Grinch27@outlook.com>
2024-09-09 13:40:14 +0800

    Create novnc project

---
+++ novnc/push/tag_version.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Get current directory
+dir_current=$(dirname "$(readlink -f "$0")")
+echo "dir_current=${dir_current}"
+
+echo "tag_version=${tag_version}"
+tag_version=$(echo "${tag_version}" | cut -d'-' -f2-)
+echo "tag_version=${tag_version}"
