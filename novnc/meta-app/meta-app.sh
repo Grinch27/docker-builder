b429a643420d3bdcee3b4bb05f2f9a563415d879
Grinch27 <Grinch27@outlook.com>
2024-09-09 15:16:06 +0800

    Update meta-app.sh

--- novnc/meta-app/meta-app.sh
+++ novnc/meta-app/meta-app.sh
@@ -4,8 +4,17 @@
 dir_current=$(dirname "$(readlink -f "$0")")
 echo "dir_current=${dir_current}"
 
-# version=$(warp-cli --version)
-# remove_prefix="warp-cli "
+# version=$(noVNC --version)
+# remove_prefix="noVNC "
 # version=${version##*$remove_prefix}
-version="4.17.7"
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update --ignore-missing
+apt-get install -y --no-install-recommends -qq \
+    ca-certificates \
+    gpg \
+    curl \
+    lsb-release
+URL_noVNC=$(curl -s https://api.github.com/repos/novnc/noVNC/releases/latest | grep 'tarball_url' | cut -d ':' -f 2- | cut -d '"' -f 2)
+version=$(echo "${URL_noVNC}" | awk -F/ '{print $NF}' | cut -d 'v' -f 2-)
 echo "version=${version}"
