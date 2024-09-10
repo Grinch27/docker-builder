0fc0d8be950c808e6b9a0755d0978d0cf72710b0
Grinch27 <Grinch27@outlook.com>
2024-09-10 19:35:04 +0800

    Update tag_version.sh

--- novnc_alpine/push/tag_version.sh
+++ novnc_alpine/push/tag_version.sh
@@ -5,5 +5,6 @@ dir_current=$(dirname "$(readlink -f "$0")")
 echo "dir_current=${dir_current}"
 
 echo "tag_version=${tag_version}"
-tag_version=$(echo "${tag_version}" | cut -d'-' -f2-)
+# tag_version=$(echo "${tag_version}" | cut -d'-' -f2-)
+tag_version=""
 echo "tag_version=${tag_version}"
