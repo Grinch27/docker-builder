eddf447453a716b3af8d188a6da7a3192fb873ff
Grinch27 <Grinch27@outlook.com>
2024-09-15 17:00:07 +0800

    Update test.sh

--- steamcmd/test/test.sh
+++ steamcmd/test/test.sh
@@ -8,6 +8,7 @@ echo "dir_current=${dir_current}"
 # remove_prefix="warp-cli "
 # version=${version##*$remove_prefix}
 # echo "version=${version}"
-# ls -lh /home/steam/
-# steamcmd
+
 apt list
+# ls -lh /home/steam/
+steamcmd
