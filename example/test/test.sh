39d6b5aa97a2265788e81001e5db17aff1a99333
Grinch27 <Grinch27@outlook.com>
2024-09-15 15:29:39 +0800

    Create test.sh

---
+++ example/test/test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Get current directory
+dir_current=$(dirname "$(readlink -f "$0")")
+echo "dir_current=${dir_current}"
+
+apt list
+
+# version=$(app --version)
+# remove_prefix="app "
+# version=${version##*$remove_prefix}
+# echo "version=${version}"
