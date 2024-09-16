a2b115a5ea1d9ff46479a9033eda36dd85647eb9
Grinch27 <Grinch27@outlook.com>
2024-09-16 13:44:49 +0800

    Update meta-app.sh

--- example/meta-app/meta-app.sh
+++ example/meta-app/meta-app.sh
@@ -4,7 +4,8 @@
 dir_current=$(dirname "$(readlink -f "$0")")
 echo "dir_current=${dir_current}"
 
-apt list
+apt list --installed
+dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n
 
 version=""
 echo "version=${version}"
