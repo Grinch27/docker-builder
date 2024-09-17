4055c6bcce972d15ad94249b1f02d6aaba722b4c
Grinch27 <Grinch27@outlook.com>
2024-09-17 18:00:47 +0800

    Update meta-app.sh

--- application/protonvpn/meta-app/meta-app.sh
+++ application/protonvpn/meta-app/meta-app.sh
@@ -4,8 +4,17 @@
 dir_current=$(dirname "$(readlink -f "$0")")
 echo "dir_current=${dir_current}"
 
-apt list --installed
-dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n
+export DEBIAN_FRONTEND=noninteractive
+apt-get update --ignore-missing
+apt-get install -y --no-install-recommends -qq \
+    ca-certificates \
+    gpg \
+    curl
 
-version=""
+gpg_file="/usr/share/keyrings/protonvpn_public.gpg"
+curl -fsSL https://repo.protonvpn.com/debian/public_key.asc | gpg --yes --dearmor --output ${gpg_file}
+echo "deb [signed-by=${gpg_file}] https://repo.protonvpn.com/debian stable main" > /etc/apt/sources.list.d/protonvpn.list
+apt-get update --ignore-missing -y -qq
+
+version=$(apt-cache madison protonvpn-stable-release | awk -F '|' '{print $2}' | tr -d ' ' | head -n 1)
 echo "version=${version}"
