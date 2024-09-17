25697a14075163c5e944c5c90175ed01dc6db0ed
Grinch27 <Grinch27@outlook.com>
2024-09-17 19:53:14 +0800

    Update build.sh

--- application/runner/build/build.sh
+++ application/runner/build/build.sh
@@ -4,12 +4,22 @@
 dir_current=$(dirname "$(readlink -f "$0")")
 echo "dir_current=${dir_current}"
 
-# 搜索 Dockerfile 文件，不区分大小写
-dockerfile=$(find "${dir_current}" -type f -iname 'dockerfile')
-if [ -z "$dockerfile" ]; then
-  echo "未找到 Dockerfile 文件"
+default_dockerfile="Dockerfile"
+# 检查环境变量 dockerfile 是否已被定义
+if [ -z "$dockerfile_name" ]; then
+  echo "环境变量 dockerfile 未定义，使用默认值: ${default_dockerfile}"
+  dockerfile_name=${default_dockerfile}
 else
-  echo "找到 Dockerfile 文件: ${dockerfile}"
+  echo "环境变量 dockerfile 已定义: ${dockerfile_name}"
+fi
+
+# 当前目录下搜索 Dockerfile 文件，不区分大小写
+dockerfile=$(find "${dir_current}" -type f -iname "${dockerfile_name}")
+if [ -z "$dockerfile" ]; then
+  echo "未找到Dockerfile文件 ${dockerfile_name}"
+  exit 1
+else
+  echo "找到Dockerfile文件 ${dockerfile_name}"
 fi
 
 # docker buildx
