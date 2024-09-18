f80c6d6e8b30e88145c970a5336dabbad424eadc
Grinch27 <Grinch27@outlook.com>
2024-09-18 11:12:24 +0800

    Update build.sh

--- application/runner/build/build.sh
+++ application/runner/build/build.sh
@@ -4,17 +4,16 @@
 dir_current=$(dirname "$(readlink -f "$0")")
 echo "dir_current=${dir_current}"
 
-default_dockerfile="Dockerfile"
 # 检查环境变量 dockerfile 是否已被定义
-if [ -z "$dockerfile_name" ]; then
-  echo "环境变量 dockerfile_name 未定义，使用默认值: ${default_dockerfile}"
-  dockerfile_name=${default_dockerfile}
+if [ -z "$dockerfile" ]; then
+  dockerfile="Dockerfile"
+  echo "环境变量 dockerfile 未定义，使用默认值: ${dockerfile}"
 else
-  echo "环境变量 dockerfile_name 已定义: ${dockerfile_name}"
+  echo "环境变量 dockerfile 已定义: ${dockerfile}"
 fi
 
 # 当前目录下搜索 Dockerfile 文件，不区分大小写
-dockerfile=$(find "${dir_current}" -type f -iname "${dockerfile_name}")
+dockerfile=$(find "${dir_current}" -type f -iname "${dockerfile}")
 if [ -z "$dockerfile" ]; then
   echo "未找到Dockerfile文件 ${dockerfile}"
   exit 1
