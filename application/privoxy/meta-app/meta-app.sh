9ba11c0c9535fa3f5034997e0ae4854ac19b7a03
Grinch27 <Grinch27@outlook.com>
2024-09-22 21:39:53 +0800

    Create meta-app.sh

---
+++ application/privoxy/meta-app/meta-app.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Get current directory
+dir_current=$(dirname "$(readlink -f "$0")")
+echo "dir_current=${dir_current}"
+
+apk update -q
+apk add --no-cache -q \
+    ca-certificates \
+    privoxy
+
+version=$(privoxy --version)
+version=$(echo "$version" | awk '/Privoxy version / {print $3}')
+echo "version=${version}"
