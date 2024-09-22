127815df14d977dd97ca4c164cf3710338c807cc
Grinch27 <Grinch27@outlook.com>
2024-09-22 21:43:16 +0800

    Create test.sh

---
+++ application/privoxy/test/test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Get current directory
+dir_current=$(dirname "$(readlink -f "$0")")
+echo "dir_current=${dir_current}"
+
+version=$(privoxy --version)
+version=$(echo "$version" | awk '/Privoxy version / {print $3}')
+echo "version=${version}"
