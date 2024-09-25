8a2ac53815af4467a22581f33cac51d1f0e63a24
Grinch27 <Grinch27@outlook.com>
2024-09-25 11:22:07 +0800

    Update meta-app.sh

--- application/adguardvpn-cli/meta-app/meta-app.sh
+++ application/adguardvpn-cli/meta-app/meta-app.sh
@@ -35,7 +35,9 @@ esac
 # ----- load build_arg -----
 set -x
 export flag_split=","
-export build_arg=$(echo "${build_arg}" | sed 's|^[\"]*||;s|[\"]*$||' | sed "s|^[\']*||;s|[\']*$||")
+export build_arg=$(echo "${build_arg}" | sed "s|^[\']*||;s|[\']*$||")
+echo "${build_arg}"
+export build_arg=$(echo "${build_arg}" | sed "s|^[\']*||;s|[\']*$||")
 echo "${build_arg}"
 echo "${build_arg}" | tr "${flag_split}" '\n' | while IFS='=' read -r key value; do
     if [ -n "$key" ] && [ -n "$value" ]; then
