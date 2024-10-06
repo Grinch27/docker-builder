eb80ad4cfcbc42c6a43361630195de0f3498221d
Grinch27 <Grinch27@outlook.com>
2024-10-06 16:10:31 +0800

    Update meta-app.sh

--- application/vlmcsd/meta-app/meta-app.sh
+++ application/vlmcsd/meta-app/meta-app.sh
@@ -7,8 +7,12 @@ echo "dir_current=${dir_current}"
 apk add --no-cache -q git make build-base
 git clone --branch master --single-branch https://github.com/Wind4/vlmcsd.git
 cd vlmcsd/
+
+# check repo version
 git describe --tags --long
 # svn info
+
+# make
 make
 cp bin/vlmcsd /usr/bin/vlmcsd
 
