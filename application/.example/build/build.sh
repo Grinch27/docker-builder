b351ccebe83c83d8c2c6dc8824874b4a364e1537
Grinch27 <Grinch27@outlook.com>
2024-10-08 22:35:18 +0800

    Update build.sh

--- application/.example/build/build.sh
+++ application/.example/build/build.sh
@@ -6,8 +6,11 @@ set -x
 dir_current=$(dirname "$(readlink -f "$0")")
 echo "dir_current=${dir_current}"
 
-# 搜索 Dockerfile 文件，不区分大小写
-dockerfile=$(find "${dir_current}" -type f -iname 'dockerfile')
+# 如果未定义 dockerfile 变量，则设置默认值
+dockerfile=${dockerfile:-dockerfile}
+
+# 使用忽略大小写的完全匹配搜索
+dockerfile=$(find "${dir_current}" -type f -iname "${dockerfile}")
 if [ -z "$dockerfile" ]; then
   echo "未找到 Dockerfile 文件"
   exit 1
@@ -18,7 +21,7 @@ fi
 # ===== docker buildx =====
 docker buildx build \
   --platform ${arch_branch} \
-  --file ${dockerfile} \
+  --file "${dockerfile}" \
   --tag "${registry_local}/${repo_dockerhub}:${tag_repo}" \
   --build-arg base_image=${base_image} \
   ${build_arg:+--build-arg build_arg=${build_arg}} \
