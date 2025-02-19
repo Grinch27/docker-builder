73e92b5713c6451bfe230d90dfece2c8dcade7c7
Grinch27 <Grinch27@outlook.com>
2025-02-19 23:02:51 +0800

    更新版本提取逻辑，添加示例以便于理解版本格式

--- application/samba/push/tag_version.sh
+++ application/samba/push/tag_version.sh
@@ -7,3 +7,5 @@ echo "dir_current=${dir_current}"
 echo "tag_version=${tag_version}"
 tag_version=$(echo "${tag_version}" | cut -d'-' -f2-)
 echo "tag_version=${tag_version}"
+
+# 4.21.3-Debian-4.21.3+dfsg-6+b1
