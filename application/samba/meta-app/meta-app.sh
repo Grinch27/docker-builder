bcb39eb1f0eb014e9f6d2ccbced789f584c9a1ab
Grinch27 <Grinch27@outlook.com>
2025-02-19 23:04:29 +0800

    更新安装命令，改用apt-get替代apk以支持Debian环境

--- application/samba/meta-app/meta-app.sh
+++ application/samba/meta-app/meta-app.sh
@@ -4,31 +4,35 @@
 dir_current=$(dirname "$(readlink -f "$0")")
 echo "dir_current=${dir_current}"
 
-apk update -q
-apk add --no-cache -q \
-    ca-certificates \
-    samba \
-    samba-client \
-    samba-client-libs \
-    samba-common \
-    samba-common-server-libs \
-    samba-common-tools \
-    # samba-dc \
-    # samba-dc-libs \
-    # samba-dev \
-    # samba-doc \
-    # samba-libnss-winbind \
-    samba-libs \
-    # samba-libs-py3 \
-    # samba-pidl \
-    samba-server \
-    samba-server-libs \
-    # samba-server-openrc \
-    # samba-test \
-    samba-util-libs
-    # samba-winbind \
-    # samba-winbind-clients \
-    # samba-winbind-krb5-locator \
+# apk update -q
+# apk add --no-cache -q \
+#     ca-certificates \
+#     samba \
+#     samba-client \
+#     samba-client-libs \
+#     samba-common \
+#     samba-common-server-libs \
+#     samba-common-tools \
+#     # samba-dc \
+#     # samba-dc-libs \
+#     # samba-dev \
+#     # samba-doc \
+#     # samba-libnss-winbind \
+#     samba-libs \
+#     # samba-libs-py3 \
+#     # samba-pidl \
+#     samba-server \
+#     samba-server-libs \
+#     # samba-server-openrc \
+#     # samba-test \
+#     samba-util-libs
+#     # samba-winbind \
+#     # samba-winbind-clients \
+#     # samba-winbind-krb5-locator \
+apt-get update --ignore-missing -y -qq
+apt-get install --no-install-recommends --no-install-suggests --fix-missing -y -qq \
+	ca-certificates \
+	samba
 
 # Version 4.20.6
 # 4.21.3-Debian-4.21.3+dfsg-6+b1
