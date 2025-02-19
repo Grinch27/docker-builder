c249dfad192aa25fb38285111b8203cb91eba916
Grinch27 <Grinch27@outlook.com>
2025-02-19 21:57:05 +0800

    优化版本提取逻辑，简化版本号处理并修复标签版本输出

--- application/samba/meta-app/meta-app.sh
+++ application/samba/meta-app/meta-app.sh
@@ -31,6 +31,13 @@ apk add --no-cache -q \
     # samba-winbind-krb5-locator \
 
 # Version 4.20.6
+# 4.21.3-Debian-4.21.3+dfsg-6+b1
 version=$(smbd --version)
-version=$(echo "$version" | awk '/Version/ {print $2}')
+# version=$(echo "$version" | awk '/Version/ {print $2}')
+# echo "version=${version}"
+
+remove_prefix="Version"
+remove_suffix="-Debian-"
+version=${version##*$remove_prefix}
+version=${version%%$remove_suffix*}
 echo "version=${version}"
