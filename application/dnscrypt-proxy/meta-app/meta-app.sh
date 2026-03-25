83511174185278a88da54cf9f802f8fdc4c6760f
grinch27 <grinch27@outlook.com>
2026-03-26 07:36:07 +0800

    重构Dockerfile和相关脚本，使用Alpine替代Debian，优化依赖安装流程

--- application/dnscrypt-proxy/meta-app/meta-app.sh
+++ application/dnscrypt-proxy/meta-app/meta-app.sh
@@ -4,8 +4,8 @@
 dir_current=$(dirname "$(readlink -f "$0")")
 echo "dir_current=${dir_current}"
 
-apt-get update --ignore-missing -y -qq
-apt-get install --no-install-recommends --no-install-suggests --fix-missing -y -qq \
+apk update -q
+apk add --no-cache -q \
     ca-certificates \
     dnscrypt-proxy
 
