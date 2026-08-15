be02180a1a1f3477012324f2719f8f7b36335c18
grinch27 <grinch27@outlook.com>
2026-08-15 22:05:59 +0800

    fix: stabilize channel assets and target versions

--- application/doh-nosni/test/test.sh
+++ application/doh-nosni/test/test.sh
@@ -4,14 +4,9 @@
 dir_current=$(dirname "$(readlink -f "$0")")
 echo "dir_current=${dir_current}"
 
-# # Version 4.20.6
-# # 4.21.3-Debian-4.21.3+dfsg-6+b1
-# version=$(smbd --version)
-# # version=$(echo "$version" | awk '/Version/ {print $2}')
-# # echo "version=${version}"
-
-# remove_prefix="Version "
-# remove_suffix="-Debian"
-# version=${version##*$remove_prefix}
-# version=${version%%$remove_suffix*}
-# echo "version=${version}"
+version=$(dnscrypt-proxy -version)
+if [ -z "${version}" ]; then
+    echo "Unable to determine dnscrypt-proxy version" >&2
+    exit 1
+fi
+echo "version=${version}"
